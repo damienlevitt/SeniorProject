68ca0b8d547d50eab76178ea013b7619a28e411d
NECKTER <nick.scheele@yahoo.com>
2020-11-18 23:58:51 -0800

    actuator test update

--- ns_actuator_test.py
+++ ns_actuator_test.py
@@ -1,22 +1,47 @@
 import linear_driver as ld
 import time
 
-# Test the left leg up.
+# Right leg
 ld.extend(1)
 time.sleep(2)
+ld.stay(1)
+time.sleep(2)
 ld.contract(1)
+time.sleep(2)
+ld.stay(1)
 
-# Test the left leg down.
+#wait
+time.sleep(2)
+
+# Left Leg
 ld.extend(2)
 time.sleep(2)
+ld.stay(2)
+time.sleep(2)
 ld.contract(2)
+time.sleep(2)
+ld.stay(2)
 
-# Test the right leg up.
+#wait
+time.sleep(2)
+
+# Middle Leg
 ld.extend(3)
 time.sleep(2)
+ld.stay(3)
+time.sleep(2)
 ld.contract(3)
+time.sleep(2)
+ld.stay(3)
 
-# Test the right leg down.
+#wait
+time.sleep(2)
+
+# All Legs
 ld.extend(4)
 time.sleep(2)
+ld.stay(4)
+time.sleep(2)
 ld.contract(4)
+time.sleep(2)
+ld.stay(4)
