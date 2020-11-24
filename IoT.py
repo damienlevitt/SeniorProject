ad3a9724862a6be8aac22bd03508b6e19fdd8d99
damienlevitt <damienlevitt@yahoo.com>
2020-11-23 18:07:54 -0800

    Revised IoT connection

--- IoT.py
+++ IoT.py
@@ -14,19 +14,16 @@ def publish_status(id, run_event, retry = 5):
     # Will need to change when updating IoT Core
     IoT_CLIENT = "Self-Leveling-Device" + str(id)
     HOST_NAME = "a2d34x14spudxd-ats.iot.us-west-1.amazonaws.com"
-    ROOT_CA = "/home/pi/certs/AmazonRootCA1.pem"
-    PRIVATE_KEY = "/home/pi/certs/4d62ec6825-private.pem.key"
-    CERT_FILE = "/home/pi/certs/4d62ec6825-certificate.pem.crt"
-    myMQTTClient = AWSIoTMQTTClient(IoT_CLIENT)
+    ROOT_CA = "certs/AmazonRootCA1.pem"
+    PRIVATE_KEY = "certs/4d62ec6825-private.pem.key"
+    CERT_FILE = "certs/4d62ec6825-certificate.pem.crt"
+    myMQTTClient = AWSIoTMQTTClient.AWSIoTMQTTClient(IoT_CLIENT)
     myMQTTClient.configureEndpoint(HOST_NAME, 8883)
     myMQTTClient.configureCredentials(ROOT_CA, PRIVATE_KEY, CERT_FILE)
-    myMQTTClient.configureOfflinePublishQueueing(-1)
-    myMQTTClient.configureDrainingFrequency(2)
-    myMQTTClient.configureConnectDisconnectTimeout(10)
-    myMQTTClient.configureMQTTOperationTimeout(10)
     myMQTTClient.connect()
-
-    for i in range (5):
+    print("connected")
+    for i in range(5):
+        print("in loop")
         AWSIoTMQTTClient.publish("iot/selfLevel", acc.is_level(), 1)
         print("Published: level status to the topic: " + "'iot/selfLevel'")
         time.sleep(2)
